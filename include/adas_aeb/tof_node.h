b156a69339f891221ff1e9739e56a2c886233569
Kacper Wołosik <63367300+mnb1188@users.noreply.github.com>
2022-05-09 18:43:27 +0200

    Add files via upload

--- include/adas_aeb/tof_node.h
+++ include/adas_aeb/tof_node.h
@@ -1,6 +1,7 @@
-#include "ros/ros.h"
-#include "ros/console.h"
-#include "std_msgs/Float32.h
+#include <ros/ros.h>
+#include <ros/console.h>
+#include <std_msgs/Float32.h>
+#include "setup_port.h"
 
 #pragma once
 
@@ -8,11 +9,12 @@ class Tof_node {
 
     private:
         ros::Publisher pub;
-        ros::Subscriber dist_subscriber;
+        ros::Subscriber dist_sub;
+        Setup my_port;
 
     public:
-        Tof_node(ros::NodeHandle);
-        void dist_callback(const std_msgs::Float32& )
+        Tof_node(ros::NodeHandle*);
+        void dist_callback(const std_msgs::Float32&);
 
 
 };
