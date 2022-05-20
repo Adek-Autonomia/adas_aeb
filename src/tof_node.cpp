d25d6c1abef8268aedbf6fe6d25c388d9d5be115
mnb1188 <kacperwolosik@onet.pl>
2022-05-20 11:52:26 +0200

    added func descryptions

--- src/tof_node.cpp
+++ src/tof_node.cpp
@@ -3,31 +3,22 @@
 #include "setup_port.h"
 #include "tokenizer.h"
 
+/**
+ * @brief Initializes new ros node rosponsible for sending distance data from ToF sensors
+ * 
+ * @param nh ros::NodeHandle object used as a parent element
+ */
 Tof_node::Tof_node(ros::NodeHandle *nh)
 {
     pub = nh->advertise<std_msgs::Float32>("/distance", 10);
     my_port.SetEverything();
 
 }
-/*
-void Tof_node::dist_callback(const std_msgs::Float32& msg) {
-    
-    std::string data = my_port.ReadInput();
-    std::vector<std::string> first_try;
-    std::vector<std::vector<float>> second_try;
 
-
-    first_try = Tokenizer::tokenize(&data, "\n");
-
-    Pipeline pipeline(first_try);
-    
-    second_try = pipeline.ProcessData();
-    
-    std_msgs::Float32 new_msg;
-    new_msg.data = second_try[0][1];
-    pub.publish(new_msg);
-}
-*/
+/**
+ * @brief Publishes gathered data
+ * 
+ */
 void Tof_node::publish_dist(){
     std::string data = my_port.ReadInput();
     std::cout << "Input size: " << data.size() << "\n";
