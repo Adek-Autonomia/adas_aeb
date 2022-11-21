cbf85cfa1479ae16db228833e2bd1469d92bd3d2
MKlimek00 <klimekmaciej00@gmail.com>
2022-11-21 15:57:35 +0100

    ADAS-200 feat: z axis filtering

--- src/pcl_aeb/cloud_filter.cpp
+++ src/pcl_aeb/cloud_filter.cpp
@@ -25,17 +25,45 @@ void CloudFilter::callback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
     pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2;
     pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-    pcl::PCLPointCloud2* cloud_filtered = new pcl::PCLPointCloud2;
-    pcl::PCLPointCloud2Ptr cloudFilteredPtr (cloud_filtered);
 
     // Convert to PCL data type
     pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+    pcl::PCLPointCloud2* cloud_filtered = new pcl::PCLPointCloud2;
+    pcl::PCLPointCloud2Ptr cloudFilteredPtr (cloud_filtered);
+
+    //downsampling
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud(cloudPtr);
+    sor.setLeafSize(0.01, 0.01, 0.01); //to be moved to cfg file
+    sor.filter(*cloudFilteredPtr);
+
+    pcl::PointCloud<pcl::PointXYZRGB> *xyz_cloud = new pcl::PointCloud<pcl::PointXYZRGB>;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr xyzCloudPtr(xyz_cloud);
+
+    pcl::fromPCLPointCloud2(*cloudFilteredPtr, *xyzCloudPtr);
+
+    //removing street
+    pcl::PointCloud<pcl::PointXYZRGB> *xyz_cloud_filtered = new pcl::PointCloud<pcl::PointXYZRGB>;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr xyzCloudPtrFiltered (xyz_cloud_filtered);
+
+    pcl::PassThrough<pcl::PointXYZRGB> pass;
+    pass.setInputCloud(xyzCloudPtr);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(-0.02, 0.35); //to be moved to cfg
+    pass.filter(*xyzCloudPtrFiltered);
+
+    pcl::PCLPointCloud2 outputPCL;
+    pcl::toPCLPointCloud2(*xyzCloudPtrFiltered, outputPCL);
+    //
+    // tbc
+    //
 
     sensor_msgs::PointCloud2* output = new sensor_msgs::PointCloud2;
     sensor_msgs::PointCloud2ConstPtr  outputPtr(output);
 
     // Convert back to ros msg
-    pcl_conversions::fromPCL(*cloud, *output);
+    pcl_conversions::fromPCL(outputPCL, *output);
     
     output->header = cloud_msg->header;
 
